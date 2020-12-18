5b2fb630f9ecb70c61022956a20a5587734f6e78
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-12-18 19:47:45 +0100

    optimize imports

--- src/main/java/com/enderzombi102/emr/EnviromineRefurbished.java
+++ src/main/java/com/enderzombi102/emr/EnviromineRefurbished.java
@@ -1,34 +1,33 @@
 package com.enderzombi102.emr;
 
+import com.enderzombi102.emr.advancement.Advancements;
 import com.enderzombi102.emr.handler.RegistrationHandler;
 import net.fabricmc.api.*;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @EnvironmentInterface(value = EnvType.CLIENT, itf = ClientModInitializer.class)
 public class EnviromineRefurbished implements ClientModInitializer, ModInitializer {
 
-	public static EnviromineRefurbished instance = null;
+	public static final Logger LOGGER = LogManager.getLogger("EMR");
+
 	public static final ItemGroup EnviroTab = FabricItemGroupBuilder.build(
 			new Identifier("emr", "envirotab"),
 			() -> new ItemStack( Content.airMask )
 	);
 
-	public EnviromineRefurbished() {
-		instance = this;
-	}
-
-
 	@Override
 	public void onInitialize() {
 		RegistrationHandler.registerItems();
+		Advancements.initAdvancements();
 	}
 
 	@Environment(EnvType.CLIENT)
 	@Override
 	public void onInitializeClient() {
-
 	}
 }
