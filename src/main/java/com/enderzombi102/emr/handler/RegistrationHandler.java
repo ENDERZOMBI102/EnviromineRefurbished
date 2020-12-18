ae18d69ad9b9cf801c9df9b8c51b97d8c8c573b9
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-12-18 19:43:24 +0100

    TheThing Sounds

--- src/main/java/com/enderzombi102/emr/handler/RegistrationHandler.java
+++ src/main/java/com/enderzombi102/emr/handler/RegistrationHandler.java
@@ -1,21 +1,28 @@
 package com.enderzombi102.emr.handler;
 
 import com.enderzombi102.emr.Content;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class RegistrationHandler {
 
 	public static void registerItems() {
 		Registry.register( Registry.ITEM,"emr:air_filter", Content.airFilter);
+		Registry.register( Registry.ITEM, "emr:rotten_food", Content.rottenFood );
+		// armor
 		Registry.register( Registry.ITEM, "emr:air_mask", Content.airMask );
 		Registry.register( Registry.ITEM, "emr:camel_pack", Content.camelPack );
 		Registry.register( Registry.ITEM, "emr:hard_hat", Content.hardHat );
 		Registry.register( Registry.ITEM, "emr:gas_mask", Content.gasMask );
-		Registry.register( Registry.ITEM, "emr:rotten_food", Content.rottenFood );
 		// bottles
 		Registry.register( Registry.ITEM, "emr:bad_water_bottle", Content.badWaterBottle);
 		Registry.register( Registry.ITEM, "emr:salty_water_bottle", Content.saltWaterBottle);
 		Registry.register( Registry.ITEM, "emr:cold_water_bottle", Content.coldWaterBottle);
+	}
+
+	public static void registerSounds() {
+		Registry.register( Registry.SOUND_EVENT, Content.SOUND_THE_THING_ID, Content.SOUND_THE_THING );
 	}
 
 }
