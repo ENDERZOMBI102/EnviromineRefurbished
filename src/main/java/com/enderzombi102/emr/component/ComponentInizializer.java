f9c3cd31d777320ad9f31f09c4d229aceffed912
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-12-18 19:47:08 +0100

    suppressed experimental warning

--- src/main/java/com/enderzombi102/emr/component/ComponentInizializer.java
+++ src/main/java/com/enderzombi102/emr/component/ComponentInizializer.java
@@ -11,7 +11,7 @@ import static com.enderzombi102.emr.Content.PLAYER_DATA_TRACKER;
 public class ComponentInizializer implements EntityComponentInitializer {
 
 	@Override
-	@SuppressWarnings({"unchecked"})
+	@SuppressWarnings({"unchecked", "experimental"})
 	public void registerEntityComponentFactories(EntityComponentFactoryRegistry registry) {
 		registry.registerForPlayers(PLAYER_DATA_TRACKER, PlayerDataTracker::new);
 	}
